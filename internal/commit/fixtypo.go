5e40632b604db681b963ebadce8ddc71bcab1a23
alirezaarzehgar <alirezaarzehgar82@gmail.com>
2025-04-16 21:59:39 +0330

    feature(internal/commit): Add function to fix commit message
    Add a function to fix commit messages using an external language model.
    The function takes a commit message as input and returns a fixed commit message.
    It also commits the changes.
    
    Signed-off-by: alirezaarzehgar <alirezaarzehgar82@gmail.com>

---
+++ internal/commit/fixtypo.go
@@ -0,0 +1,21 @@
+package commit
+
+import (
+	"fmt"
+
+	"github.com/alirezaarzehgar/git-llm/internal/llm"
+)
+
+func FixCommitMessage(langModel llm.LanguageModel, message string) error {
+	commitMessage, err := langModel.FixComment(message)
+	if err != nil {
+		return fmt.Errorf("failed to connect LLM: %w", err)
+	}
+
+	err = gitCommit(commitMessage)
+	if err != nil {
+		return fmt.Errorf("failed to commit: %w", err)
+	}
+
+	return nil
+}
